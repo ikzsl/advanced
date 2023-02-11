23b0207f90037a54b8ae2028a71c716aa22377c2
Джалалудинов Муслим <dzhalaludinov@proitr.ru>
2023-02-11 19:26:42 +0300

    decompose webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import webpack from 'webpack';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { BuildOptions } from './types/config';
+
+export const buildWebpackConfig = (
+  options: BuildOptions
+): webpack.Configuration => {
+  const { paths, mode } = options;
+  return {
+    mode,
+    entry: paths.entry,
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+  };
+};
